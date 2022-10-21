c2f62126aa05e2fac1c106afd331c11bc349c542
SetiadiKusuma <kusumasetiadi@gmail.com>
2022-10-21 22:25:13 +0700

    First Commit

---
+++ cypress/integration/searchbox.test.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe('Searchbox Test', function() {
+    before(() => {
+        cy.visit('http://zero.webappsecurity.com/index.html');
+    })
+    
+    it('Should type into searchbox and submit', () => {
+        cy.get('#searchTerm').type('online {enter}')
+    })
+
+    it('Should show search result page', () => {
+        cy.get('h2').should('contain.text', 'Search Results:')
+
+        cy.get('a[href="/online-banking.html"]')
+            .should('have.length', 1)
+            .and('have.text', 'Zero - Free Access to Online Banking')
+        
+        cy.get('a[href="/bank/online-statements.html"]')
+            .should('have.length', 1)
+            .and('have.text', 'Zero - Online Statements')
+    })
+
+});
